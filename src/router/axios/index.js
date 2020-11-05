fb380f451a0c28d3842df8aace8db63fc23e913d
Lucas Oliveira <hattori_@outlook.com>
2020-11-05 18:05:37 -0300

    fix cors

--- src/router/axios/index.js
+++ src/router/axios/index.js
@@ -2,10 +2,9 @@
 import axios from 'axios'
 
 const baseURL = process.env.VUE_APP_SIMULATOR_API
-const headers = { 'Content-Type':'application/x-www-form-urlencoded'}
 const responseType = 'json'
 
-const instance = axios.create({ baseURL, headers, responseType })
+const instance = axios.create({ baseURL, responseType })
 
 export default instance
 
