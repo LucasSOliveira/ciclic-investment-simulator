3a812080d0599ca5c15bfe08a34e77fe3c4fd908
Lucas Oliveira <hattori_@outlook.com>
2020-11-05 19:49:19 -0300

    fix axios, remove line

--- src/router/axios/index.js
+++ src/router/axios/index.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 const baseURL = process.env.VUE_APP_SIMULATOR_API
 const responseType = 'json'
-
 const instance = axios.create({ baseURL, responseType })
 
 export default instance
